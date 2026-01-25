da27a22afa805b7ba12dd1236bda2346538710e7
tro1q <toriqul6812@gmail.com>
2026-01-25 13:46:12 +0600

    error fix

--- Dashboard/Entities/Member.cs
+++ Dashboard/Entities/Member.cs
@@ -9,17 +9,20 @@ namespace Dashboard.Entits
     internal class Member
     {
 
-        public int MId { get; set; }
-        public string MName { get; set; }
-        public string MGen { get; set; }
-        public DateTime MDOB { get; set; }
-        public DateTime MJDate { get; set; }
-        public int MMembership { get; set; }
-        public int MCoach { get; set; }
-        public string MPhone { get; set; }
-        public string MTiming { get; set; }
-        public string MStatus { get; set; }
-        public bool IsDeleted { get; set; }
+        public int MId { get; set; }         
+        public string MName { get; set; }      
+        public string MGen { get; set; }       
+        public DateTime MDOB { get; set; }     
+        public DateTime MJDate { get; set; }  
+        public int MMembership { get; set; }  
+        public int MCoach { get; set; }        
+        public string MPhone { get; set; }     
+        public string MTiming { get; set; }    
+        public string MStatus { get; set; }   
+        public bool IsDeleted { get; set; }    
+
+        
+        public Member() { }
 
     }
 }
